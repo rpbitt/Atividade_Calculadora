37e7c5412ed7a2171e1c3de13ba0eee487b93279
rpbitt <91853994+rpbitt@users.noreply.github.com>
2022-05-14 15:36:10 -0300

    Correção erro 4
    Correção do 4 erro listado no mantis.

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ export default function App() {
         setCurrentNumber((fistNumber * lastNumber).toString());
         return;
       case '/':
-        setCurrentNumber((fistNumber - lastNumber).toString());
+        setCurrentNumber((fistNumber / lastNumber).toString());
         return;
   }
 
